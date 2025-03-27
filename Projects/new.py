a0c16ec05f15f9f802ad4d922912c9df83536c1e
ShivamGupta7700 <shivam.pcmsi@gmail.com>
2025-03-27 19:52:19 +0530

    ok

---
+++ Projects/new.py
@@ -0,0 +1,40 @@
+# # n = int(input('>> '))
+# # a =0
+# # b =1 
+
+# # l = [0,1]
+# # i = 0 
+# # while i <=  n-2:
+# #     c =a+b
+# #     l.append(c)
+# #     a = b
+# #     b=c
+# #     i += 1
+# # print(l)
+
+# p = {
+#     'p1':1003,
+#     'p2':1030,
+#     'p3':1400,
+#     'p4':500,
+
+# }
+# l=[]
+# for key , value in p.items():
+#     l.append(value)
+
+# for key in p:
+#     if p[key] == max(l):
+#         print(
+#             p.get(key)
+#         )
+
+
+# x = 10
+# x=x+10
+# x -= 5
+# print(x)
+# # x,y = x-2,22
+# # print(x,y)
+
+# print('Hello World'.split('o'))
