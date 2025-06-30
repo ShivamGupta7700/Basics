15a1209c5cf861464045f137434259d63b28c66c
ShivamGupta7700 <shivam.pcmsi@gmail.com>
2025-06-30 14:15:04 +0530

    test

---
+++ file/binaryFile.py
@@ -0,0 +1,13 @@
+import pickle
+
+try:
+    with open('sample.dat', 'wb+') as file :
+        data = {
+                'name':'Shivam',
+                  "age":17,
+                }
+        
+        pickle.dump(data, file)
+        
+except Exception as e:
+    print(f'Error >> {e}')
--- file/readandwrite.py
+++ file/readandwrite.py
@@ -1,3 +1,17 @@
-file = open("file/sample.txt", "a+")
-file.write("I am Elon Musk")
-file.close()
+# with open("file/sample.txt", "w+") as file:
+#     print(file.tell())
+    
+#     file.write('I am shivam gupta')
+#     file.seek(0)
+#     print(file.readline())
+#     print(file.tell())
+a = 0
+with open("file/sample.txt", "r") as file:
+    lines = file.readlines()
+    for words in lines:
+        for letter in words:
+            if letter in 'aeiou':
+                a+=1
+
+print(a)
+
