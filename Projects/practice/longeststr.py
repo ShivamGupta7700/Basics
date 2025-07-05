032c1324fb88a8db190f7b0fed7dff6b779a2a92
ShivamGupta7700 <shivam.pcmsi@gmail.com>
2025-07-05 16:34:48 +0530

    leetcode

--- Projects/practice/longeststr.py
+++ Projects/practice/longeststr.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 class Solution:
     def isPalindrome(self, x):
         x = str(x)
@@ -18,17 +20,47 @@ class Solution:
                 return 'Lucky'
             else:
                 return -1
-    def findLucky(self, arr):
+    def findLucky11(self, arr):
         l = []
         for a in arr:
             if a == arr.count(a):
                 l.append(a)
         a = max(l) if len(l) >0 else -1
-
         return a
-
+    def findLucky(self, arr):
+        return max((x for x, f in Counter(arr).items() if x==f), default=-1)
+    
+    def removeDuplicates(self, nums):
+        nums.sort()
+        a = [x for x ,y in Counter(nums).items() if len(nums) > 0]
+        a.sort()
+        del nums[:]
+        nums.extend(a)
+        k = len(nums)
+        return k
+    
+    def strStr(self, haystac:str, needle:str):
+        a = haystac.find(needle)
+        return a 
+    
+    def searchInsert(self, nums, target):
+        for i in range(len(nums)):
+            if nums[i] == target:
+                return i
+        else:
+            if max(nums) < target:
+                    return len(nums)
+            else:
+                z = 0
+                for a in nums:
+                    if target < a:
+                        return z
+                    z +=1
+    def reverse(self, x):
+        str(x)
+        
+            
+                
 
 c = Solution()
-c.isPalindrome(121)
-print(c.removeElement([3,2,2,3],3))
-print(c.findLucky([2,2,2,3,3]))
+print(c.searchInsert([1,3,5,6],7))
