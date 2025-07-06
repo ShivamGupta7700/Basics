43e810d587eacac7e9e91dbc755bbbb52e7203aa
ShivamGupta7700 <shivam.pcmsi@gmail.com>
2025-07-06 09:57:00 +0530

    linkedList

---
+++ Algorithms/linked_list.py
@@ -0,0 +1,8 @@
+class node:
+    def __init__(self, data=None , next = None):
+        self.data = data
+        self.next = next 
+
+class Linked_list:
+    def __init__(self):
+        self.head = None
