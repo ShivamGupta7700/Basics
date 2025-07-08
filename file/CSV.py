b5c917f2b9fd4c27721cd03ceb2a052cb292bd8f
ShivamGupta7700 <shivam.pcmsi@gmail.com>
2025-07-08 13:43:23 +0530

    Refactor CSV file handling to read data instead of writing

--- file/CSV.py
+++ file/CSV.py
@@ -1,10 +1,13 @@
 import csv
 
 try:
-    with open('sample.csv', 'w', newline='') as file:
-        fileObj = csv.writer(file, delimiter=',')
-        l = ['shivam','17','Good','Male']
-        fileObj.writerow(l)
+    with open('sample.csv', 'r+', newline='') as file:
+        # fileObj = csv.writer(file, delimiter=',')
+        # l = ['shivam','17','Good','Male']
+        # fileObj.writerow(l)
+        fileread = csv.reader(file)
+        for row in fileread:
+            print(row)
 
 except Exception as e:
     print(f'Error >> {e}')
