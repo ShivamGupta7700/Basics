e036fbed6c7c750a149a087c3d6dd2bb32358a44
ShivamGupta7700 <shivam.pcmsi@gmail.com>
2025-07-23 14:14:20 +0530

    Update sample.txt content and modify with.py to read and write correctly

--- file/with.py
+++ file/with.py
@@ -1,5 +1,8 @@
-with open("file/sample.txt", 'r') as f:
+with open("file/sample.txt", 'r+') as f:
+    f.seek(6, 0)
     data = f.read()
     print(data)
+    print(f.tell())
+    
 with open("file/sample.txt", 'w') as f:
     f.write('Hello I am Shivam')
