781c60821d6948110f2ff35ede2bd544abcfff98
ShivamGupta7700 <shivam.pcmsi@gmail.com>
2025-07-24 20:54:37 +0530

    Comment out frequency calculation code in frequency.py

--- Projects/basics/frequency.py
+++ Projects/basics/frequency.py
@@ -1,5 +1,9 @@
-from collections import Counter
-lst = list(map(int, input("Enter numbers: ")))
-print(lst)
-freq = Counter(lst)
-print("Frequencies:", freq)
+# from collections import Counter
+# lst = list(map(int, input("Enter numbers: ")))
+# print(lst)
+# freq = Counter(lst)
+# print("Frequencies:", freq)
+
+
+
+print(''+'abcs')
