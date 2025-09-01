3d517c44f52921af4fab5f1c0a92adf6da744acd
ShivamGupta7700 <shivam.pcmsi@gmail.com>
2025-09-01 18:07:49 +0530

    Refactor CSV.py to comment out unused CSV reading code and update sample.dat handling

--- file/CSV.py
+++ file/CSV.py
@@ -1,13 +1,26 @@
 import csv
+import pickle
+# try:
+#     with open('sample.csv', 'r+', newline='') as file:
+#         # fileObj = csv.writer(file, delimiter=',')
+#         # l = ['shivam','17','Good','Male']
+#         # fileObj.writerow(l)
+#         fileread = csv.reader(file)
+#         for row in fileread:
+#             print(row)
 
-try:
-    with open('sample.csv', 'r+', newline='') as file:
-        # fileObj = csv.writer(file, delimiter=',')
-        # l = ['shivam','17','Good','Male']
-        # fileObj.writerow(l)
-        fileread = csv.reader(file)
-        for row in fileread:
-            print(row)
+# except Exception as e:
+#     print(f'Error >> {e}')
 
-except Exception as e:
-    print(f'Error >> {e}')
+with open('sample.dat', 'ab') as file:
+    pickle.dump(['emp1','IT',2000],file)
+
+
+
+with open('sample.dat', 'rb+') as file:
+    l = pickle.load(file)
+    print(l)
+    for a in l:
+        if a[1] == 'IT':
+            a[2] = 3000
+    pickle.dump(l, file)
