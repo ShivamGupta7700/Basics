f99686892346ba32e870e6b2e0445770871d75d0
ShivamGupta7700 <shivam.pcmsi@gmail.com>
2025-09-22 19:08:16 +0530

    Add implementation of singly linked list with node creation and display functionality

---
+++ DSA/linkedList.py
@@ -0,0 +1,49 @@
+# singly linked list 
+
+
+class singlyNode:
+    
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next 
+
+    def __str__(self):
+        return str(self.value)
+    
+
+
+# Now we have to set data and makes the connection
+
+Head = singlyNode(120)
+A = singlyNode(420)
+B = singlyNode(100)
+C = singlyNode(500)
+
+# Now for the connection 
+
+Head.next = A
+A.next = B
+B.next =C
+
+print(A)
+
+
+curr = Head 
+while curr:
+    print(curr)
+    curr = curr.next 
+
+
+
+# Now for displaying linked list - O(n)
+def display(head):
+    curr = head
+    elements = []
+    while curr:
+        elements.append(str(curr.value))
+        curr = curr.next
+    print('  -> '.join(elements))
+
+display(Head)
+
+# Searching for a node !
